0c6fb503ddaba05b8e33d2031db403c829ecb1ff
Julio Pedrosa <japm@cin.ufpe.br>
2018-12-28 00:32:30 -0200

    start building food app

---
+++ foodapp/food.app/src/pages/category/category.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-category',
+  templateUrl: 'category.html',
+})
+export class CategoryPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CategoryPage');
+  }
+
+  openProduct(): void{
+    this.navCtrl.push('ProductPage');
+  }
+
+}
